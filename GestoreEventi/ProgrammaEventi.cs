783feaa975ec8e2a1d53e0f76824afe91c48b027
SimoneMondello <mondellosimo@gmail.com>
2022-04-22 15:07:48 +0200

    parte3.DaFinire

---
+++ GestoreEventi/ProgrammaEventi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class ProgrammaEventi
+    {
+        public string titolo;
+        public List<Evento> Eventi;
+
+
+        public ProgrammaEventi
+            (string titolo, List<Evento> Eventi)
+        {
+            this.titolo = titolo;
+            List<Evento> list = new List<Evento>();
+        }
+
+
+        
+    }
+    
+}
+
+    
+
