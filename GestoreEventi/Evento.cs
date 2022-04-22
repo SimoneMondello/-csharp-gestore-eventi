e5cca202a2b8916f1512e4e30a0e04021a11ea35
SimoneMondello <mondellosimo@gmail.com>
2022-04-22 12:47:54 +0200

    prima.parte

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        public string titolo;
+        public double data;
+        public int postiTotale;
+        public int postiPrenotati;
+
+        public int BigliettiRestanti;
+        public int BigliettiDisdetti;
+
+        public Evento(string titolo, double data, int postiTotale, int postiPrenotati)
+        {
+            this.titolo = titolo;
+            this.data = data;
+            this.postiTotale = postiTotale;
+            this.postiPrenotati = postiPrenotati;
+        }
+
+        public string GetTitolo()
+        {
+            Console.WriteLine(titolo);
+            return titolo;
+        }
+        public double GetData()
+        {
+            return data = 0;
+
+        }
+        public int GetpostiTotale()
+        {
+            return postiTotale = 200;
+        }
+        public int GetpostiPrenotati()
+        {
+            return postiPrenotati;
+        }
+
+        public int GetBigliettiDisdetti()
+        {
+            return BigliettiDisdetti;
+        }
+
+
+        public void PrenotaPosti()
+
+
+        {
+            for (int i = 0; i < postiTotale; i++)
+            {
+                postiTotale = 200;
+                if (postiTotale == i) ;
+
+                Console.WriteLine("Il numero di posti totale è" + postiTotale);
+
+
+                Console.WriteLine("Inserisci quanti posti vuoi prenotare");
+                postiPrenotati = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Grazie per aver prenotato numero posti :" + postiPrenotati);
+                postiTotale = postiTotale - postiPrenotati;
+                Console.WriteLine(postiTotale);
+                return;
+            }
+        }
+
+
+
+        public void DisdiciPosti()
+
+
+        {
+            for (int i = 0; i < postiTotale; i++)
+            {
+                BigliettiRestanti = postiTotale;
+                Console.WriteLine("I biglietti restanti sono " + BigliettiRestanti);
+
+                Console.WriteLine("Quanti posti vuoi disdire?");
+
+                BigliettiDisdetti = int.Parse(Console.ReadLine());
+                postiTotale = BigliettiDisdetti + postiTotale;
+                Console.WriteLine("I biglietti disponibili sono" + postiTotale)
+                ;
+                return;
+
+
+            }
+
+
+
+        }
+    }
+}
+
+
+ /*
+            public override string ToString()
+            {
+                string rappresentazioneInStringa = "";
+
+                rappresentazioneInStringa += "---- Evento ---- \n";
+                rappresentazioneInStringa += "Titolo: " + this.titolo + "\n";
+                rappresentazioneInStringa += "Data: " + this.data + "\n";
+                rappresentazioneInStringa += "Posti Totale Teatro: " + this.postiTotale + "\n";
+                rappresentazioneInStringa += "Posti Prenotati: " + this.postiPrenotati + "\n";
+
+                return rappresentazioneInStringa;
+            }
+
+            public override string ToString()
+            {
+                string data = ("");
+                data += "dd/MM/yyyy";
+                return data;
+            }
+            */
+
+
+
+        
+
+
+     
+
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using GestoreEventi;
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+Evento mioEvento = new Evento("La bella e la bestia", 22/04/2022, 200, 0);
+
+mioEvento.GetTitolo();
+mioEvento.GetpostiTotale();
+mioEvento.PrenotaPosti();
+mioEvento.DisdiciPosti();
+
