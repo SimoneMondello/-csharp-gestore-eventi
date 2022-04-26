c8cc4825940739bc87716e6cf88473b31f5a3c9a
SimoneMondello <mondellosimo@gmail.com>
2022-04-26 09:11:48 +0200

    commit.metàesercizio

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -69,6 +69,7 @@ namespace GestoreEventi
                 postiTotale = postiTotale - postiPrenotati;
                 Console.WriteLine(postiTotale);
                 return;
+
             }
         }
 
